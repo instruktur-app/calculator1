3374e54b9a8c50d05e6bc0f3de187a330a92306b
DillaCarolineK <81397833+DillaCarolineK@users.noreply.github.com>
2022-10-07 19:34:46 +0700

    add multiple function

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function pengurangan (bilanganA, bilanganB){
     return bilanganA - bilanganB;
 }
 
+const perkalian = (bilanganA, bilanganB) => bilanganA*bilanganB
+
 
 var A = 99;
 var B = 40;
@@ -14,3 +16,4 @@ var kurang = pengurangan(A, B);
 console.log (penjumlahan (10,20))
 
 console.log ("hasil pengurangan dari "+ A + " dan " + B + " adalah " + kurang);
+console.log (perkalian(10,5));
